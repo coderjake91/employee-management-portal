d160dd474ebea6c2c2566c2415c7e21ada5fc86c
coderjake91 <jacob.b.frazer@gmail.com>
2021-12-04 21:03:03 -0600

    added inquirer prompts and menu logic to the CLI application, cleaned up some of the query methods in Interface.js

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
-/*
-Psuedocode for CLI interface (using inquirer)
+//(Interface Testing)=====================================
 
-On app start,
+//const Interface = require('./lib/Interface');
+//const interface = new Interface();
 
-instantiate Interface
+//Employee portal test data
+//
+// const roleObj = {
+//     title: 'Paralegal',
+//     salary: '40000.00',
+//     department_id: '2'
+// }
 
-Present inquirer options menu 
-(
-->view all departments 
-    on select: (interface.allDepartments)
-->view all roles 
-    on select: (interface.allRoles)
-->view all employees (interface.allEmployees)
-->add a department (interface.addDepartment)
-->add a role (interface)
-->add an employeee
-->update an employee role
-)
-*/
-//test data
-const roleObj = {
-    title: 'Paralegal',
-    salary: '40000.00',
-    department_id: '2'
-}
+// const employeeObj = {
+//     first_name: 'Jake',
+//     last_name: 'Frazer',
+//     role_id: '4',
+//     manager_id: '2'
+// }
 
-const employeeObj = {
-    first_name: 'Jake',
-    last_name: 'Frazer',
-    role_id: '4',
-    manager_id: '2'
-}
+// const updateRoleObj = {
+//     id: '6',
+//     role_id: '7'
+// }
 
-const updateRoleObj = {
-    id: '6',
-    role_id: '7'
-}
-
-const Interface = require('./lib/Interface');
-
-const interface = new Interface();
-
-interface.printMenuHeading('Employee Portal');
-// console.log('----------------------------')
+//interface.printMenuHeading('Employee Portal');
 //interface.allDepartments();
-// console.log('----------------------------')
 //interface.allRoles();
-// console.log('----------------------------')
 //interface.allEmployees();
-// console.log('----------------------------')
 //interface.addDepartment('Research');
-// console.log('----------------------------')
 // interface.addRole(roleObj);
-// console.log('----------------------------')
 //interface.addEmployee(employeeObj);
-// console.log('----------------------------')
 //interface.updateRole(updateRoleObj);
+
+//(Interface Testing)=====================================
+
+const CommandLinePrompts = require('./lib/CLI');
+const artwork = require('ascii-art');
+
+const employeePortal = new CommandLinePrompts;
+
+
+artwork.font("Employee Portal", 'doom')
+    .then((heading) => {
+        console.log(heading);
+        employeePortal.optionMenu();
+     });
